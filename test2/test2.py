88e6b2847e55b4b55291822af6dc5f80cce31705
Mike Weiblen <mew@mew.cx>
2023-07-09 02:38:37 -0600

    test2.py : frame out support for 3 sensors

--- test2/test2.py
+++ test2/test2.py
@@ -8,19 +8,41 @@ import busio
 from digitalio import DigitalInOut
 from adafruit_pn532.spi import PN532_SPI
 
-spi = busio.SPI(clock=board.GP18, MOSI=board.GP19, MISO=board.GP16)
-cs = DigitalInOut(board.GP17)
-pn532 = PN532_SPI(spi, cs, debug=False)
+spi = busio.SPI(clock=board.GP18, MOSI=board.GP19, MISO=board.GP20)
 
-ic, ver, rev, support = pn532.firmware_version
-print("PN532 firmware: {1}.{2} ic: {0} support: {3}\n".format(ic, ver, rev, support))
+SS1 = DigitalInOut(board.GP17)
+SS2 = DigitalInOut(board.GP21)
+SS3 = DigitalInOut(board.GP5)
 
-pn532.SAM_configuration()
+RF1 = PN532_SPI(spi, SS1, debug=False)
+RF2 = PN532_SPI(spi, SS2, debug=False)
+RF3 = PN532_SPI(spi, SS3, debug=False)
 
-print("Waiting for RFID/NFC card...")
+print("RF1.firmware_version ", RF1.firmware_version)
+print("RF2.firmware_version ", RF2.firmware_version)
+print("RF3.firmware_version ", RF3.firmware_version)
+
+RF1.SAM_configuration()
+RF2.SAM_configuration()
+RF3.SAM_configuration()
+
 while True:
-    uid = pn532.read_passive_target(timeout=0.5)
-    if uid is None:
-        print(".", end="")
+    uid = RF1.read_passive_target(timeout=0.5)
+    if uid:
+        print([hex(i) for i in uid], " ", end="")
     else:
-        print("\ncard UID:", [hex(i) for i in uid])
+        print("RF1 ", end="")
+
+    uid = RF2.read_passive_target(timeout=0.5)
+    if uid:
+        print([hex(i) for i in uid], " ", end="")
+    else:
+        print("RF2 ", end="")
+
+    uid = RF3.read_passive_target(timeout=0.5)
+    if uid:
+        print([hex(i) for i in uid], " ", end="")
+    else:
+        print("RF3 ", end="")
+
+    print(" $")
