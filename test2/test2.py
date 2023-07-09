30aa0bad08d92722642b4acdac7634fb2173811f
Mike Weiblen <mew@mew.cx>
2023-07-09 11:30:15 -0600

    test2.py

--- test2/test2.py
+++ test2/test2.py
@@ -5,6 +5,7 @@
 
 import board
 import busio
+import time
 from digitalio import DigitalInOut
 from adafruit_pn532.spi import PN532_SPI
 
@@ -14,35 +15,38 @@ SS1 = DigitalInOut(board.GP17)
 SS2 = DigitalInOut(board.GP21)
 SS3 = DigitalInOut(board.GP5)
 
+print("RF1 init")
 RF1 = PN532_SPI(spi, SS1, debug=False)
+print("RF2 init")
 RF2 = PN532_SPI(spi, SS2, debug=False)
+print("RF3 init")
 RF3 = PN532_SPI(spi, SS3, debug=False)
 
+SENSORS = [RF1, RF2, RF3]
+
+print("version query")
 print("RF1.firmware_version ", RF1.firmware_version)
 print("RF2.firmware_version ", RF2.firmware_version)
 print("RF3.firmware_version ", RF3.firmware_version)
 
+print("configuring")
 RF1.SAM_configuration()
 RF2.SAM_configuration()
 RF3.SAM_configuration()
 
+def read(sensor):
+    uid = sensor.read_passive_target(timeout=0.3)
+    if uid:
+        #print([hex(i) for i in uid], " ", end="")
+        print("XXXXX ", end="")
+    else:
+        print("      ", end="")
+    sensor.power_down()
+    time.sleep(0.1)
+
 while True:
-    uid = RF1.read_passive_target(timeout=0.5)
-    if uid:
-        print([hex(i) for i in uid], " ", end="")
-    else:
-        print("RF1 ", end="")
+    read(RF1)
+    read(RF2)
+    read(RF3)
+    print("")
 
-    uid = RF2.read_passive_target(timeout=0.5)
-    if uid:
-        print([hex(i) for i in uid], " ", end="")
-    else:
-        print("RF2 ", end="")
-
-    uid = RF3.read_passive_target(timeout=0.5)
-    if uid:
-        print([hex(i) for i in uid], " ", end="")
-    else:
-        print("RF3 ", end="")
-
-    print(" $")
