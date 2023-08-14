5048298fa297d6ba005e482e9864951b304883ba
Mike Weiblen <mew@mew.cx>
2023-08-13 21:35:31 -0600

    test3/test3.py : code cleanup after demo, 0.3.1.0
    rm commented-out lines
    
    comments and code reorg (ver 0.3.1.0)

--- test3/test3.py
+++ test3/test3.py
@@ -1,23 +1,37 @@
-# test3.py 2023-08-13
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
 # SPDX-FileCopyrightText: 2023 Mike Weiblen http://mew.cx/
 #
 # SPDX-License-Identifier: MIT
+#
+# rfid_experiment/test3/test3.py
+# Read NTAG21x RFID tags using up to seven PN532 sensor modules.
+# Indicate which sensors are detecting tags using an LED strip.
+# Part of the Sono Chapel position-sensing experiments.
+# Demonstrated 2023-08-13
+
 
 import board
 import busio
 import time
+import adafruit_dotstar
 from digitalio import DigitalInOut
 from adafruit_pn532.spi import PN532_SPI
-import adafruit_dotstar
 
+__repo__ = "https://github.com/itchy-o/rfid_experiment"
+__version__ = "0.3.1.0"
+
+# configure GPIO pins for LED strip, turn all LEDs off.
 leds = adafruit_dotstar.DotStar(board.GP26, board.GP27, 8, brightness=0.2)
+RED   = 0xff0000
+GREEN = 0x00ff00
+BLUE  = 0x0000ff
+OFF   = 0
+leds.fill(OFF)
+
+# configure GPIO pins for Serial Peripheral Interface (SPI).
 spi = busio.SPI(clock=board.GP18, MOSI=board.GP19, MISO=board.GP20)
 
-#leds.fill(0xff0000); time.sleep(0.1)
-#leds.fill(0x00ff00); time.sleep(0.1)
-#leds.fill(0x0000ff); time.sleep(0.1)
-leds.fill(0)
-
+# list of GPIO pins controling the sensors' SPI chip-select.
 SENSORS = (
         board.GP9,
         board.GP10,
@@ -31,19 +45,16 @@ SENSORS = (
 PN = []
 
 def rf_init(i,ss):
-    #print("init ", i)
     leds[i] = 0x00ff00
-    #ss = SENSORS[i]
     ssio = DigitalInOut(ss)
     dev = PN532_SPI(spi, ssio, debug=False)
-    print(i, "firmware_version(", i, ") = ", dev.firmware_version)
+    print("firmware_version(", i, ") = ", dev.firmware_version)
     leds[i] = 0x0000ff
     dev.SAM_configuration()
     leds[i] = 0
     PN.append(dev)
 
 def read(i,dev):
-    #print(">> ", i, " - ", dev)
     uid = dev.read_passive_target(timeout=0.3)
     if uid:
         leds[i] = 0xff0000
@@ -53,17 +64,13 @@ def read(i,dev):
     #time.sleep(0.1)
 
 def init():
-    leds.fill(0)
     for i,ss in enumerate(SENSORS):
         rf_init(i,ss)
 
 def main():
     init()
-    #print("len(PN) = ", len(PN))
-
     while True:
         for i,dev in enumerate(PN):
-            #print(i)
             read(i,dev)
 
 main()
