a9785306c89aec8315a85343da1c8dff77944fed
Mike Weiblen <mew@mew.cx>
2023-08-14 00:07:18 -0600

    code reorg, DotStar, LED colors
    comments and code reorg (led colors)
    
    comma
    
    BLUE if uid is None else RED
    
    DotStar

--- test3/test3.py
+++ test3/test3.py
@@ -4,73 +4,87 @@
 # SPDX-License-Identifier: MIT
 #
 # rfid_experiment/test3/test3.py
+# Developed using CircuitPython 8.x on Raspberry Pi Pico MCU.
 # Read NTAG21x RFID tags using up to seven PN532 sensor modules.
 # Indicate which sensors are detecting tags using an LED strip.
 # Part of the Sono Chapel position-sensing experiments.
-# Demonstrated 2023-08-13
-
+# Demonstrated at iOHQ 2023-08-13
 
 import board
 import busio
 import time
-import adafruit_dotstar
 from digitalio import DigitalInOut
 from adafruit_pn532.spi import PN532_SPI
+from adafruit_dotstar import DotStar
 
 __repo__ = "https://github.com/itchy-o/rfid_experiment"
 __version__ = "0.3.1.0"
 
-# configure GPIO pins for LED strip, turn all LEDs off.
-leds = adafruit_dotstar.DotStar(board.GP26, board.GP27, 8, brightness=0.2)
+# Configure GPIO for 8-LED strip, turn all LEDs off.
+leds  = DotStar(clock=board.GP26, data=board.GP27, n=8, brightness=0.5)
+WHITE = 0xffffff
 RED   = 0xff0000
 GREEN = 0x00ff00
 BLUE  = 0x0000ff
-OFF   = 0
-leds.fill(OFF)
+BLACK = 0
+leds.fill(BLACK)
 
-# configure GPIO pins for Serial Peripheral Interface (SPI).
+# Configure GPIO for Serial Peripheral Interface (SPI).
 spi = busio.SPI(clock=board.GP18, MOSI=board.GP19, MISO=board.GP20)
 
-# list of GPIO pins controling the sensors' SPI chip-select.
-SENSORS = (
+# The GPIO pins controling each sensor's SPI chip-select (CS).
+CS_GPIOS = (
         board.GP9,
         board.GP10,
         board.GP11,
         board.GP12,
         board.GP13,
         board.GP14,
-        board.GP15
+        board.GP15,
 )
 
-PN = []
+# List of handles to initialized/configured sensor instances.
+# Contents are generated during sensor_init().
+SENSORS = []
 
-def rf_init(i,ss):
-    leds[i] = 0x00ff00
-    ssio = DigitalInOut(ss)
-    dev = PN532_SPI(spi, ssio, debug=False)
-    print("firmware_version(", i, ") = ", dev.firmware_version)
-    leds[i] = 0x0000ff
-    dev.SAM_configuration()
-    leds[i] = 0
-    PN.append(dev)
+def sensor_init(i, cs_gpio):
+    """Initialize a PN532 RFID sensor.
+    Show progress on-screen and the sensor's LED
+    """
+    # Create a SPI driver instance for the PN532 sensor.
+    leds[i] = GREEN
+    dio = DigitalInOut(cs_gpio)
+    sensor = PN532_SPI(spi, dio, debug=False)
+    print("firmware_version ", i, " = ", sensor.firmware_version)
 
-def read(i,dev):
-    uid = dev.read_passive_target(timeout=0.3)
-    if uid:
-        leds[i] = 0xff0000
-    else:
-        leds[i] = 0x0000ff
-    dev.power_down()
+    # Configure the sensor
+    leds[i] = BLUE
+    sensor.SAM_configuration()
+    SENSORS.append(sensor)
+    leds[i] = BLACK
+
+def sensor_read(i, sensor):
+    """Set the sensor's LED to indicate if the sensor detected a
+    tag or not.
+    """
+    leds[i] = WHITE
+    uid = sensor.read_passive_target(timeout=0.3)
+    leds[i] = BLUE if uid is None else RED
+    sensor.power_down()
     #time.sleep(0.1)
 
-def init():
-    for i,ss in enumerate(SENSORS):
-        rf_init(i,ss)
+def init_all():
+    for i, cs_gpio in enumerate(CS_GPIOS):
+        sensor_init(i, cs_gpio)
+
+def read_all():
+    for i, sensor in enumerate(SENSORS):
+        sensor_read(i, sensor)
 
 def main():
-    init()
+    init_all()
     while True:
-        for i,dev in enumerate(PN):
-            read(i,dev)
+        read_all()
 
+# Make it so...
 main()
