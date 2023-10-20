509604ee4657bfbd626289a37d84b649f8061ad6
Mike Weiblen <mew@mew.cx>
2023-10-20 16:58:27 -0600

    temp code*

---
+++ test4/code3.py
@@ -0,0 +1,7 @@
+import q2
+
+print("start")
+for i,k in enumerate(q2.tuid):
+    print(i,k)
+print("done")
+
---
+++ test4/code_.py
@@ -0,0 +1,4 @@
+# code.py
+
+from mew_keybow_pico.keybow_pico_cew_editkeys import main
+main()
