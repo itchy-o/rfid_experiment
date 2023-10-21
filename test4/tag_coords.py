0ca0709eb0603b22b9cfcbc1d348b83720be6cd5
Mike Weiblen <mew@mew.cx>
2023-10-20 20:15:36 -0600

    tag_coords.py : sort -u c0.log > tag_coords.py

--- test4/tag_coords.py
+++ test4/tag_coords.py
@@ -1,4 +1,281 @@
-data = {}
+
+]0;🐍Wi-Fi: off | 123@adafruit_pn532/spi.py KeyboardInterrupt | 8.2.6\
+]0;🐍Wi-Fi: off | 128@adafruit_pn532/spi.py KeyboardInterrupt | 8.2.6\
+]0;🐍Wi-Fi: off | 46@adafruit_pn532/spi.py KeyboardInterrupt | 8.2.6\
+]0;🐍Wi-Fi: off | Done | 8.2.6\]0;🐍Wi-Fi: off | REPL | 8.2.6\
+]0;🐍Wi-Fi: off | Done | 8.2.6\soft reboot
+]0;🐍Wi-Fi: off | REPL | 8.2.6\
+]0;🐍Wi-Fi: off | code.py | 8.2.6\042a785a1a1390
+]0;🐍Wi-Fi: off | code.py | 8.2.6\042a815a1a1390
+]0;🐍Wi-Fi: off | code.py | 8.2.6\PN532 firmware: 1.6 ic: 50 support: 7
+]0;🐍Wi-Fi: off | code.py | 8.2.6\Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "adafruit_pn532/adafruit_pn532.py", line 235, in _write_frame
+  File "adafruit_pn532/adafruit_pn532.py", line 310, in send_command
+  File "adafruit_pn532/adafruit_pn532.py", line 329, in process_response
+  File "adafruit_pn532/adafruit_pn532.py", line 382, in read_passive_target
+  File "adafruit_pn532/adafruit_pn532.py", line 386, in read_passive_target
+  File "adafruit_pn532/adafruit_pn532.py", line 400, in listen_for_passive_target
+  File "adafruit_pn532/adafruit_pn532.py", line 418, in get_passive_target
+  File "adafruit_pn532/spi.py", line 123, in _wait_ready
+  File "adafruit_pn532/spi.py", line 128, in _wait_ready
+  File "adafruit_pn532/spi.py", line 151, in <listcomp>
+  File "adafruit_pn532/spi.py", line 151, in _write_data
+  File "adafruit_pn532/spi.py", line 46, in reverse_bit
+  File "assign_xy.py", line 23, in main
+  File "code.py", line 2, in <module>
+  File "code.py", line 9, in <module>
+  File "tidreader.py", line 26, in read
+ ---- UNKNOWN
+042a615a1a1390
+042a625a1a1390
+042a635a1a1390
+042a645a1a1390
+042a655a1a1390
+042a665a1a1390
+042a675a1a1390
+042a685a1a1390
+042a695a1a1390
+042a6a5a1a1390
+042a6b5a1a1390
+042a6c5a1a1390
+042a6d5a1a1390
+042a6e5a1a1390
+042a705a1a1390
+042a715a1a1390
+042a725a1a1390
+042a735a1a1390
+042a745a1a1390
+042a755a1a1390
+042a765a1a1390
+042a775a1a1390
+042a795a1a1390
+042a7a5a1a1390
+042a7b5a1a1390
+042a7c5a1a1390
+042a7d5a1a1390
+042a7e5a1a1390
+042a7f5a1a1390
+042a805a1a1390
+042a825a1a1390
+042a845a1a1390
+042a855a1a1390
+042a865a1a1390
+042a875a1a1390
+042a885a1a1390
+042a895a1a1390
+042a8a5a1a1390
+042a8b5a1a1390
+042a8c5a1a1390
+042a8d5a1a1390
+042a8e5a1a1390
+042a8f5a1a1390
+042a9a5a1a1390
+042a9b5a1a1390
+042a9c5a1a1390
+042a9d5a1a1390
+042a9e5a1a1390
+042c835a1a1390
+042c855a1a1390
+042c865a1a1390
+042c875a1a1390
+042c885a1a1390
+042c895a1a1390
+042c8a5a1a1390
+042c8b5a1a1390
+042c8c5a1a1390
+042c8d5a1a1390
+042c8e5a1a1390
+042c8f5a1a1390
+042c905a1a1390
+042c915a1a1390
+042c925a1a1390
+042cb15a1a1390
+042cb25a1a1390
+042cb35a1a1390
+042cde5a1a1390
+042d995a1a1390
+042d9a5a1a1390
+042d9b5a1a1390
+042d9c5a1a1390
+042d9d5a1a1390
+042d9e5a1a1390
+042d9f5a1a1390
+042da05a1a1390
+042da15a1a1390
+042da25a1a1390
+042dae5a1a1390
+042daf5a1a1390
+042db05a1a1390
+042db15a1a1390
+042db25a1a1390
+042db35a1a1390
+042db45a1a1390
+042db55a1a1390
+042db65a1a1390
+042db75a1a1390
+042db85a1a1390
+042db95a1a1390
+042dba5a1a1390
+042dbb5a1a1390
+042dbd5a1a1390
+042dbe5a1a1390
+042dbf5a1a1390
+042dc05a1a1390
+042dc15a1a1390
+042dc25a1a1390
+042dc45a1a1390
+042dc55a1a1390
+042dc65a1a1390
+042dc75a1a1390
+042dd75a1a1390
+042dd85a1a1390
+042dd95a1a1390
+042dda5a1a1390
+042ddb5a1a1390
+042ddc5a1a1390
+042ddd5a1a1390
+042dde5a1a1390
+042ddf5a1a1390
+530a6343510001
+530b6343510001
+530c6343510001
+53116343510001
+53126343510001
+53136343510001
+53146343510001
+53196343510001
+531a6343510001
+531b6343510001
+531c6343510001
+53216343510001
+53226343510001
+53236343510001
+53246343510001
+53475a44510001
+53485a44510001
+534a6343510001
+534b6343510001
+534c6343510001
+53526343510001
+53536343510001
+53546343510001
+53596343510001
+536c6343510001
+53716343510001
+53726343510001
+53736343510001
+53746343510001
+53796343510001
+537a6343510001
+537b6343510001
+537c6343510001
+53816343510001
+53826343510001
+53836343510001
+53846343510001
+53896343510001
+538a6343510001
+538b6343510001
+538c6343510001
+53916343510001
+53926343510001
+53936343510001
+539a6343510001
+539b6343510001
+539c6343510001
+53b36343510001
+53b46343510001
+53b96343510001
+53ba6343510001
+=~=~=~=~=~=~=~=~=~=~=~= PuTTY log 2023.10.20 15:25:40 =~=~=~=~=~=~=~=~=~=~=~=
+=~=~=~=~=~=~=~=~=~=~=~= PuTTY log 2023.10.20 17:18:20 =~=~=~=~=~=~=~=~=~=~=~=
+=~=~=~=~=~=~=~=~=~=~=~= PuTTY log 2023.10.20 17:36:13 =~=~=~=~=~=~=~=~=~=~=~=
+>>> 
+>>> continue
+Adafruit CircuitPython 8.2.6 on 2023-09-12; Raspberry Pi Pico W with rp2040
+Auto-reload is on. Simply save files over USB to run them or enter REPL to disable.
+Code done running.
+KeyboardInterrupt: 
+None
+Press any key to enter the REPL. Use CTRL-D to reload.
+SyntaxError: 'continue' outside loop
+Traceback (most recent call last):
+code.py output:
+data[ '042a615a1a1390' ] =  (10, 14)
+data[ '042a625a1a1390' ] =  (10, 16)
+data[ '042a635a1a1390' ] =  (10, 18)
+data[ '042a645a1a1390' ] =  (10, 20)
+data[ '042a655a1a1390' ] =  (10, 22)
+data[ '042a665a1a1390' ] =  (9, 5)
+data[ '042a675a1a1390' ] =  (9, 7)
+data[ '042a685a1a1390' ] =  (9, 9)
+data[ '042a695a1a1390' ] =  (9, 11)
+data[ '042a6a5a1a1390' ] =  (9, 13)
+data[ '042a6b5a1a1390' ] =  (11, 17)
+data[ '042a6c5a1a1390' ] =  (11, 19)
+data[ '042a6d5a1a1390' ] =  (11, 21)
+data[ '042a6e5a1a1390' ] =  (11, 23)
+data[ '042a705a1a1390' ] =  (10, 12)
+data[ '042a715a1a1390' ] =  (12, 22)
+data[ '042a725a1a1390' ] =  (13, 21)
+data[ '042a735a1a1390' ] =  (11, 11)
+data[ '042a745a1a1390' ] =  (11, 13)
+data[ '042a755a1a1390' ] =  (11, 15)
+data[ '042a765a1a1390' ] =  (13, 19)
+data[ '042a775a1a1390' ] =  (13, 17)
+data[ '042a785a1a1390' ] =  (13, 15)
+data[ '042a795a1a1390' ] =  (13, 13)
+data[ '042a7a5a1a1390' ] =  (13, 11)
+data[ '042a7b5a1a1390' ] =  (13, 1)
+data[ '042a7c5a1a1390' ] =  (13, 3)
+data[ '042a7d5a1a1390' ] =  (13, 5)
+data[ '042a7e5a1a1390' ] =  (13, 9)
+data[ '042a7f5a1a1390' ] =  (13, 7)
+data[ '042a805a1a1390' ] =  (12, 10)
+data[ '042a815a1a1390' ] =  (12, 8)
+data[ '042a825a1a1390' ] =  (12, 6)
+data[ '042a845a1a1390' ] =  (12, 4)
+data[ '042a855a1a1390' ] =  (12, 2)
+data[ '042a865a1a1390' ] =  (7, 19)
+data[ '042a875a1a1390' ] =  (7, 21)
+data[ '042a885a1a1390' ] =  (7, 23)
+data[ '042a895a1a1390' ] =  (14, 2)
+data[ '042a8a5a1a1390' ] =  (14, 14)
+data[ '042a8b5a1a1390' ] =  (9, 15)
+data[ '042a8c5a1a1390' ] =  (9, 17)
+data[ '042a8d5a1a1390' ] =  (9, 19)
+data[ '042a8e5a1a1390' ] =  (9, 21)
+data[ '042a8f5a1a1390' ] =  (9, 23)
+data[ '042a9a5a1a1390' ] =  (11, 9)
+data[ '042a9b5a1a1390' ] =  (11, 7)
+data[ '042a9c5a1a1390' ] =  (11, 5)
+data[ '042a9d5a1a1390' ] =  (11, 3)
+data[ '042a9e5a1a1390' ] =  (11, 1)
+data[ '042c835a1a1390' ] =  (8, 14)
+data[ '042c855a1a1390' ] =  (8, 16)
+data[ '042c865a1a1390' ] =  (8, 18)
+data[ '042c875a1a1390' ] =  (8, 20)
+data[ '042c885a1a1390' ] =  (8, 22)
+data[ '042c895a1a1390' ] =  (10, 2)
+data[ '042c8a5a1a1390' ] =  (10, 8)
+data[ '042c8b5a1a1390' ] =  (10, 10)
+data[ '042c8c5a1a1390' ] =  (10, 6)
+data[ '042c8d5a1a1390' ] =  (10, 4)
+data[ '042c8e5a1a1390' ] =  (8, 4)
+data[ '042c8f5a1a1390' ] =  (8, 6)
+data[ '042c905a1a1390' ] =  (8, 8)
+data[ '042c915a1a1390' ] =  (8, 10)
+data[ '042c925a1a1390' ] =  (8, 12)
+data[ '042cb15a1a1390' ] =  (8, 2)
+data[ '042cb25a1a1390' ] =  (9, 1)
+data[ '042cb35a1a1390' ] =  (9, 3)
+data[ '042cde5a1a1390' ] =  (12, 12)
+data[ '042d995a1a1390' ] =  (14, 4)
+data[ '042d9a5a1a1390' ] =  (14, 6)
+data[ '042d9b5a1a1390' ] =  (14, 8)
+data[ '042d9c5a1a1390' ] =  (14, 10)
+data[ '042d9d5a1a1390' ] =  (14, 12)
 data[ '042d9e5a1a1390' ] =  (6, 18)
 data[ '042d9f5a1a1390' ] =  (6, 16)
 data[ '042da05a1a1390' ] =  (5, 23)
@@ -27,6 +304,15 @@ data[ '042dc45a1a1390' ] =  (2, 22)
 data[ '042dc55a1a1390' ] =  (2, 20)
 data[ '042dc65a1a1390' ] =  (2, 18)
 data[ '042dc75a1a1390' ] =  (2, 16)
+data[ '042dd75a1a1390' ] =  (14, 16)
+data[ '042dd85a1a1390' ] =  (14, 18)
+data[ '042dd95a1a1390' ] =  (14, 20)
+data[ '042dda5a1a1390' ] =  (14, 22)
+data[ '042ddb5a1a1390' ] =  (13, 23)
+data[ '042ddc5a1a1390' ] =  (12, 20)
+data[ '042ddd5a1a1390' ] =  (12, 18)
+data[ '042dde5a1a1390' ] =  (12, 16)
+data[ '042ddf5a1a1390' ] =  (12, 14)
 data[ '530a6343510001' ] =  (3, 15)
 data[ '530b6343510001' ] =  (5, 15)
 data[ '530c6343510001' ] =  (6, 14)
@@ -71,10 +357,174 @@ data[ '538c6343510001' ] =  (7, 7)
 data[ '53916343510001' ] =  (5, 7)
 data[ '53926343510001' ] =  (3, 7)
 data[ '53936343510001' ] =  (1, 7)
-data[ '539a6343510001' ] =  "BOOK1"
+data[ '539a6343510001' ] =  BOOK
 data[ '539b6343510001' ] =  (3, 3)
 data[ '539c6343510001' ] =  (3, 1)
 data[ '53b36343510001' ] =  (4, 4)
 data[ '53b46343510001' ] =  (4, 2)
 data[ '53b96343510001' ] =  (2, 4)
 data[ '53ba6343510001' ] =  (2, 2)
+enter X,Y: 
+enter X,Y: "BOOK"
+enter X,Y: 1,1
+enter X,Y: 1,11
+enter X,Y: 1,13
+enter X,Y: 1,15
+enter X,Y: 1,17
+enter X,Y: 1,19
+enter X,Y: 1,21
+enter X,Y: 1,23
+enter X,Y: 1,3
+enter X,Y: 1,5
+enter X,Y: 1,7
+enter X,Y: 1,9
+enter X,Y: 10,10
+enter X,Y: 10,12
+enter X,Y: 10,14
+enter X,Y: 10,16
+enter X,Y: 10,18
+enter X,Y: 10,2
+enter X,Y: 10,20
+enter X,Y: 10,22
+enter X,Y: 10,4
+enter X,Y: 10,6
+enter X,Y: 10,8
+enter X,Y: 11,1
+enter X,Y: 11,11
+enter X,Y: 11,13
+enter X,Y: 11,15
+enter X,Y: 11,17
+enter X,Y: 11,19
+enter X,Y: 11,21
+enter X,Y: 11,23
+enter X,Y: 11,3
+enter X,Y: 11,5
+enter X,Y: 11,7
+enter X,Y: 11,9
+enter X,Y: 12,10
+enter X,Y: 12,12
+enter X,Y: 12,14
+enter X,Y: 12,16
+enter X,Y: 12,18
+enter X,Y: 12,2
+enter X,Y: 12,20
+enter X,Y: 12,22
+enter X,Y: 12,4
+enter X,Y: 12,6
+enter X,Y: 12,8
+enter X,Y: 13,1
+enter X,Y: 13,11
+enter X,Y: 13,13
+enter X,Y: 13,15
+enter X,Y: 13,17
+enter X,Y: 13,19
+enter X,Y: 13,21
+enter X,Y: 13,24[K3
+enter X,Y: 13,3
+enter X,Y: 13,5
+enter X,Y: 13,7
+enter X,Y: 13,9
+enter X,Y: 14,10
+enter X,Y: 14,12
+enter X,Y: 14,14
+enter X,Y: 14,16
+enter X,Y: 14,18
+enter X,Y: 14,2
+enter X,Y: 14,20
+enter X,Y: 14,22
+enter X,Y: 14,4
+enter X,Y: 14,6
+enter X,Y: 14,8
+enter X,Y: 2,10
+enter X,Y: 2,12
+enter X,Y: 2,14
+enter X,Y: 2,16
+enter X,Y: 2,18
+enter X,Y: 2,2
+enter X,Y: 2,20
+enter X,Y: 2,22
+enter X,Y: 2,4
+enter X,Y: 2,6
+enter X,Y: 2,8
+enter X,Y: 3,1
+enter X,Y: 3,11
+enter X,Y: 3,13
+enter X,Y: 3,15
+enter X,Y: 3,17
+enter X,Y: 3,19
+enter X,Y: 3,21
+enter X,Y: 3,23
+enter X,Y: 3,3
+enter X,Y: 3,5
+enter X,Y: 3,7
+enter X,Y: 3,9
+enter X,Y: 4,10
+enter X,Y: 4,12
+enter X,Y: 4,14
+enter X,Y: 4,16
+enter X,Y: 4,18
+enter X,Y: 4,2
+enter X,Y: 4,20
+enter X,Y: 4,22
+enter X,Y: 4,4
+enter X,Y: 4,6
+enter X,Y: 4,8
+enter X,Y: 5,1
+enter X,Y: 5,11
+enter X,Y: 5,13
+enter X,Y: 5,15
+enter X,Y: 5,17
+enter X,Y: 5,19
+enter X,Y: 5,21
+enter X,Y: 5,23
+enter X,Y: 5,3
+enter X,Y: 5,5
+enter X,Y: 5,7
+enter X,Y: 5,9
+enter X,Y: 6,10
+enter X,Y: 6,12
+enter X,Y: 6,14
+enter X,Y: 6,16
+enter X,Y: 6,18
+enter X,Y: 6,2
+enter X,Y: 6,20
+enter X,Y: 6,22
+enter X,Y: 6,4
+enter X,Y: 6,6
+enter X,Y: 6,8
+enter X,Y: 7,1
+enter X,Y: 7,11
+enter X,Y: 7,13
+enter X,Y: 7,15
+enter X,Y: 7,17
+enter X,Y: 7,19
+enter X,Y: 7,21
+enter X,Y: 7,23
+enter X,Y: 7,3
+enter X,Y: 7,5
+enter X,Y: 7,7
+enter X,Y: 7,9
+enter X,Y: 8,10
+enter X,Y: 8,12
+enter X,Y: 8,14
+enter X,Y: 8,16
+enter X,Y: 8,18
+enter X,Y: 8,2
+enter X,Y: 8,20
+enter X,Y: 8,22
+enter X,Y: 8,4
+enter X,Y: 8,6
+enter X,Y: 8,8
+enter X,Y: 9,1
+enter X,Y: 9,11
+enter X,Y: 9,13
+enter X,Y: 9,15
+enter X,Y: 9,17
+enter X,Y: 9,19
+enter X,Y: 9,21
+enter X,Y: 9,23
+enter X,Y: 9,3
+enter X,Y: 9,5
+enter X,Y: 9,7
+enter X,Y: 9,9
+soft reboot
