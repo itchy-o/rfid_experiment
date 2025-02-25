1b1da373865359fc149327e6e8ff11412d6a1642
Mike Weiblen <mew@mew.cx>
2025-02-25 02:38:36 -0700

    code.py

--- test7/code.py
+++ test7/code.py
@@ -1,5 +1,26 @@
-from t663 import data
-print("len(data)", len(data))
-print("repr(data)", repr(data))
-print("EOF")
+import t66x
+
+print("")
+print("#SOF [")
+
+print("len(t66x.a)")
+print(len(t66x.a))
+print("")
+print("repr(t66x.a)")
+#print(repr(t66x.a))
+
+print("len(t66x.b)")
+print(len(t66x.b))
+print("")
+print("repr(t66x.b)")
+#print(repr(t66x.b))
+
+print("len(t66x.c)")
+print(len(t66x.c))
+print("")
+print("repr(t66x.c)")
+#print(repr(t66x.c))
+
+print("#EOF ]")
+print("")
 
